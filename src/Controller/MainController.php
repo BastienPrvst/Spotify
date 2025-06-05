1f84c7e3ad4c73c9d20b73dcd7df83ee88963a9c
Bastien <a@a.a>
2025-06-05 16:24:22 +0200

    Ajout de tailwind en cmd et debut du coloriage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -18,61 +24,165 @@ final class MainController extends AbstractController
     {
     }
 
-    /**
-     * @throws TransportExceptionInterface
-     */
-    #[Route('/login', name: 'app_login')]
-    public function login(): \Symfony\Component\HttpFoundation\RedirectResponse
+    #[Route('/', name: 'app_home')]
+    public function home(Request $request) : Response
     {
+        $session = $request->getSession();
+        $accessToken = $session->get('spotify_access_token', '');
+        $expiration = $session->get('spotify_access_info', '');
 
-        $redirectUri = 'http://127.0.0.1:8000/home';
+        //Création du token
+        if (!$accessToken) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        //Refresh du token si expiré
+        if ($expiration['expires_in'] < 1 ){
+            return $this->redirectToRoute('app_refresh');
+        }
+
+
+        try {
+            //infos utilisateur
+
+            $userProfile = $this->httpClient->request(
+                'GET',
+                'https://api.spotify.com/v1/me',
+                [
+                    'headers' => [
+                        'Authorization' => 'Bearer ' . $accessToken,
+                    ]
+                ]
+            )->toArray();
+
+            //top 10 artist année
+
+            $top10Artist = $this->httpClient->request(
+                'GET',
+                'https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=10',
+                [
+                    'headers' => [
+                        'Authorization' => 'Bearer ' . $accessToken,
+                    ],
+
+                ]
+            )->toArray();
+
+            //top 10 tracks année
+
+            $top10Tracks = $this->httpClient->request(
+                'GET',
+                'https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=10',
+                [
+                    'headers' => [
+                        'Authorization' => 'Bearer ' . $accessToken,
+                    ],
+                ]
+            )->toArray();
+
+            return $this->render('main/home.html.twig', [
+                'userProfile' => $userProfile,
+                'top10Tracks' => $top10Tracks,
+                'top10Artist' => $top10Artist
+            ]);
+
+        }catch (ClientExceptionInterface|RedirectionExceptionInterface|ServerExceptionInterface|DecodingExceptionInterface|TransportExceptionInterface $e) {
+            return new Response('Erreur : ' . $e->getMessage());
+        }
+
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(): RedirectResponse
+    {
+        $redirectUri = 'http://127.0.0.1:8000/authenticate';
+
         $urlData = [
             'response_type' => 'code',
             'client_id' => $_ENV['SPOTIFY_CLIENT_ID'],
             'redirect_uri' => $redirectUri,
-            'scope' => 'user-read-private user-read-email',
+            'scope' => 'user-read-private user-read-email user-top-read',
         ];
 
-        return $this->redirect("https://accounts.spotify.com/authorize" . "?" . http_build_query($urlData));
-
+        return $this->redirect("https://accounts.spotify.com/authorize?" . http_build_query($urlData));
     }
 
-    #[Route('/home', name: 'app_home')]
-    public function home(Request $request)
+
+    #[Route('/authenticate', name: 'app_authenticate')]
+    public function authenticate(Request $request): Response
     {
-        // 1. Récupération du code renvoyé par Spotify
+        if ($request->query->has('error')) {
+            return new Response('Erreur OAuth : ' . $request->query->get('error'));
+        }
+
         $code = $request->query->get('code');
-        dd($code);
+        if (!$code) {
+            return new Response('Code manquant', 400);
+        }
+
+        $redirectUri = 'http://127.0.0.1:8000/authenticate';
+        $credentials = base64_encode($_ENV['SPOTIFY_CLIENT_ID'] . ':' . $_ENV['SPOTIFY_CLIENT_SECRET']);
+
+        try {
+            $response = $this->httpClient->request(
+                'POST',
+                'https://accounts.spotify.com/api/token',
+                [
+                    'headers' => [
+                        'Content-Type' => 'application/x-www-form-urlencoded',
+                        'Authorization' => 'Basic ' . $credentials,
+                    ],
+                    'body' => http_build_query([
+                        'grant_type' => 'authorization_code',
+                        'code' => $code,
+                        'redirect_uri' => $redirectUri,
+                    ]),
+                ]
+            );
+
+            $data = $response->toArray();
+            $accessToken = $data['access_token'];
+            $request->getSession()->set('spotify_access_info', $data);
+            $request->getSession()->set('spotify_access_token', $accessToken);
+
+
+            return $this->redirectToRoute('app_home');
+
+        } catch (\Exception $e) {
+            return new Response('Erreur : ' . $e->getMessage());
+        }
     }
 
-
-    /**
-     * @throws TransportExceptionInterface
-     */
-    private function getToken(): string
+    #[Route('/refresh', name: 'app_refresh')]
+    public function refreshToken(Request $request): Response
     {
         try {
-            $request = $this->httpClient->request(
+            $response = $this->httpClient->request(
                 'POST',
                 'https://accounts.spotify.com/api/token',
                 [
                     'headers' => [
                         'Content-Type' => 'application/x-www-form-urlencoded',
                     ],
-                    'body' => http_build_query([
-                        'grant_type' => 'client_credentials',
-                        'client_id' => $_ENV['SPOTIFY_CLIENT_ID'],
-                        'client_secret' => $_ENV['SPOTIFY_CLIENT_SECRET'],
-                    ]),
+                    'body' => [
+                        'grant_type' => 'refresh_token',
+                        'refresh_token' => $request->getSession()->get('spotify_refresh_token'),
+                    ]
                 ]
             );
-        } catch (\Exception $e) {
-            return $e->getMessage();
+
+            $data = $response->toArray();
+            $accessToken = $data['access_token'];
+            $request->getSession()->set('spotify_access_info', $data);
+            $request->getSession()->set('spotify_access_token', $accessToken);
+
+            return $this->redirectToRoute('app_home');
+
+        }catch (ClientExceptionInterface|RedirectionExceptionInterface|ServerExceptionInterface|DecodingExceptionInterface|TransportExceptionInterface $e) {
+            return new Response('Erreur : ' . $e->getMessage());
         }
-
-
-        $content = $request->getContent();
-        $data = json_decode($content, true);
-        return $data['access_token'];
     }
+
+
+
 }
